ea591814c438e7d4b009a72db12ea239775ef81b
KennetSanchez <kesasaro@gmail.com>
2022-02-12 12:26:20 -0500

    Arrays added, new elements added
    There are the arrays to move between pages, and the elements that are going to be updated once the user clicks them.

---
+++ Script.js
@@ -0,0 +1,7 @@
+let routes = {};
+let templates = {};
+
+let app_section = document.getElementById("app");
+
+var section  = document.createElement("section");
+var link = document.createElement("a");
